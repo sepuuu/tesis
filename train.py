1575d47096d2ed38c929dcc490ddbe1a67909fdd
Matías Sepúlveda <71105266+sepuuu@users.noreply.github.com>
2023-10-08 04:53:25 -0300

    entrenamiento a corregir

---
+++ train.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+
+# Train the model with 2 GPUs
+
+results = model.train(data='config.yaml', epochs=1)
