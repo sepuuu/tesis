9d48d7c7f24ada7b7571ef93666359279d566a40
msepulveda <sepu.mati.19@gmail.com>
2023-10-20 01:43:28 -0300

    test

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 import ultralytics
 # Load a model
-model = YOLO('yolov8x.pt')  # load a pretrained model (recommended for training)
+model = YOLO('modelos/yolov8x.pt')  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
 #results = model.train(data='config.yaml', epochs=3 )
 
-results = model.train(data='config.yaml', epochs=1, imgsz=640, device=0,batch=-1)
+results = model.train(data='config.yaml', epochs=1, imgsz=640, device="cpu")
+
+
+
